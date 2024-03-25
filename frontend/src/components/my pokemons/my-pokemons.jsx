898e6bd397137da60d4248b129170a67c75727d9
Sudhanshu Bharti <120251980+Sudhanshu-Bharti@users.noreply.github.com>
2024-03-25 22:29:54 +0530

    api updated

--- frontend/src/components/my pokemons/my-pokemons.jsx
+++ frontend/src/components/my pokemons/my-pokemons.jsx
@@ -3,45 +3,41 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image'
 import { Button } from "../ui/button"
 import axios from "axios"
-const MyPokemons = ({pokeImg}) => {
-    const [pokemonNames, setPokemonNames] = useState([]);
-    // const [img , setImg] = useState([])
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get("/pokesprite/data/legacy/dex.json");
-            const names = response.data.map(pkmn => pkmn.slug.eng)
-            setPokemonNames(names.slice(0,30));
-          } catch (error) {
-            console.error("Error fetching data: ", error);
-          }
-        // const fetchImg = async () => {
-        //     try {
-        //         const response = await axios.get("/pokesprite/")
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // }
 
-        };
-      
-        fetchData();
-      }, []);
+const MyPokemons = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50");
+        const data = await Promise.all(response.data.results.map(async (pokemon) => {
+          const pokemonData = await axios.get(pokemon.url);
+          return { name: pokemon.name, imageUrl: pokemonData.data.sprites.front_default };
+        }));
+        setPokemons(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-
-                    <ul className="divide-y">
-                        <div className='p-2'>
-                        {pokemonNames.map((name, index) => (
-                        <Button variant="icon" key={index}>
-                             <Image src={`/pokesprite/icons/pokemon/regular/${name}.png`} width="40" height="40" />
-                             </Button>
-                     ))} 
-                        </div>
-                    </ul>
-
-  </div>
+      <ul className="divide-y">
+        <div className='p-2'>
+          {pokemons.map((pokemon, index) => (
+            <Button variant="outline" size="lg" key={index}>
+              <Image src={pokemon.imageUrl} alt={pokemon.name} width={74} height={74} />
+              
+            </Button>
+          ))}
+        </div>
+      </ul>
+    </div>
   )
 }
 
-export default MyPokemons
+export default MyPokemons;
