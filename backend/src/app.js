33b6f1b856663340f25373b739225ccf817a815f
soham haldar <sohamhaldar25@gmail.com>
2024-03-25 20:24:14 +0530

    Added Database support and other basic things

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import {createServer} from 'http';
+import {Server} from 'socket.io';
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN, 
+    credentials:true
+}));
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+const server=createServer(app);
+const io=new Server(server,{
+    cors:{
+        origin:process.env.CORS_ORIGIN,
+        credentials:true
+    }
+});
+
+// socket things
+
+//router import 
+
+//routes declaration
+
+
+export {io,server};
